965160b20d05b95ac94ca5464cd5cc663b07953d
Thuong <dangthuong1067@gmail.com>
2024-03-17 15:23:33 +0700

    bổ sung code lỗi không chuyển qua được Tab Danh mục

---
+++ src/screens/category/itemCategory/itemCategory.component.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ItemCategories = () => {
+  return (
+    <View>
+      <Text>ItemCategories</Text>
+    </View>
+  )
+}
+
+export default ItemCategories
